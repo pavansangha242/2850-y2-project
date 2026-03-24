57f0067e07b772e9a8ce4299e177e49550fe8d84
Pavan <pavan.sangha@outlook.com>
2026-03-24 22:07:43 +0000

    front end login and register

--- fitness_app/run.py
+++ fitness_app/run.py
@@ -4,11 +4,15 @@ app=Flask(__name__)
 
 @app.route("/")
 def index():
-    return render_template("home.html")
+    return render_template("login.html")
+
+@app.route("/register")
+def register():
+    return render_template("register.html")
 
 
 
 
 
-if __name__ in "__main__":
+if __name__ == "__main__":
     app.run(debug=True)
