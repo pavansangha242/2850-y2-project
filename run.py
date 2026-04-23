64ee85c2eacd80135e704a3c7d676fa98d215b49
pavansangha242 <gxpg7793@leeds.ac.uk>
2026-04-23 14:54:46 +0000

    added run file

---
+++ run.py
@@ -0,0 +1,9 @@
+from fitness_app import create_app
+from fitness_app.extensions import db
+
+app = create_app()
+
+if __name__ == "__main__":
+    with app.app_context():
+        db.create_all()
+    app.run(debug=True)
